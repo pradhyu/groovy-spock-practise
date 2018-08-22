5d8122bfeec793f57206d5e823bfa838758a69ed
PK <pradhyu.shrestha@gmail.com>
2018-08-21 22:49:19 -0400

    more test to fail with unroll

--- src/test/groovy/com/test/practise/PractiseTestSpec.groovy
+++ src/test/groovy/com/test/practise/PractiseTestSpec.groovy
@@ -12,24 +12,27 @@ import java.util.concurrent.TimeUnit
 class PractiseTestSpec extends Specification {
 
     // time out extension annotation
-    @Timeout(value=1,unit=TimeUnit.MILLISECONDS)
+    @Timeout(value=1,unit=TimeUnit.MICROSECONDS)
     @Issue("http//atlasian.com/RUL-blah-story-num")
+    @Unroll
     void "Timeout test, forced failure with 1 milliseconds" () {
-        given: "two variables x and y"
+        given: "two variables x and y ->#what"
         def x = 1
         def y = 1
-        when: "we use addition"
+        when: "we use addition function with ->#what"
         def k = x + y
-        then: " we should get sum"
+        then: " we should get sum=2"
         k == 2
+        where:" # varname in description only works for variables defined in where"
+        what << ["what","the", "hell"]
     }
 
     @Unroll
     def "maximum of #a and #b is #c"(int a, int b, int c) {
-        expect:
+        expect: "Math.max(#a #b) should the function pics maximum number #c"
         Math.max(a, b) == c
 
-        where:
+        where:"#a and #b is passed to Math.max"
         a | b | c
         1 | 3 | 3
         7 | 4 | 4
